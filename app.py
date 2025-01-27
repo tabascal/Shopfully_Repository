cb20f9339201392ccb00c204c30f2b01574c3117
tabascal <t.abascal@shopfully.com>
2025-01-27 08:21:49 +0000

    Espaciado

--- app.py
+++ app.py
@@ -183,7 +183,8 @@ ppt_template = st.file_uploader("", type=["pptx"])
 
 st.write("")  # Espaciado
 
-st.markdown("**Upload Excel File** \n*(Column A must be `Store ID`)*", unsafe_allow_html=True)
+st.markdown(
+    "**Upload Excel File** \n*(Column A must be `Store ID`)*", unsafe_allow_html=True)
 data_file = st.file_uploader("", type=["xlsx"])
 
 
